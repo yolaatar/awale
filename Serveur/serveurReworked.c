650f1498a9d97afd0680e6629f02d1802c736af8
YoussefChaouki <youschaouki@gmail.com>
2024-11-19 09:58:23 +0100

    version quasi finale

--- Serveur/serveurReworked.c
+++ Serveur/serveurReworked.c
@@ -630,42 +630,7 @@ void traiter_logout(Utilisateur *utilisateur)
     utilisateur->sock = INVALID_SOCKET;
 }
 
-void traiter_unwatch(Utilisateur *utilisateur)
-{
-    // Vérifiez si l'utilisateur est un spectateur
-    int found = 0;
-    for (int i = 0; i < nbSalons; i++)
-    {
-        Salon *salon = &salons[i];
-        for (int j = 0; j < salon->nbSpectateurs; j++)
-        {
-            if (salon->spectateurs[j] == utilisateur)
-            {
-                found = 1;
 
-                // Retirez le spectateur de la liste
-                for (int k = j; k < salon->nbSpectateurs - 1; k++)
-                {
-                    salon->spectateurs[k] = salon->spectateurs[k + 1];
-                }
-                salon->nbSpectateurs--;
-
-                // Notifiez l'utilisateur
-                write_client(utilisateur->sock, "Vous avez arrêté de regarder la partie.\n");
-
-                // Quittez les boucles après avoir supprimé l'utilisateur
-                break;
-            }
-        }
-        if (found)
-            break;
-    }
-
-    if (!found)
-    {
-        write_client(utilisateur->sock, "Erreur : Vous ne regardez aucune partie actuellement.\n");
-    }
-}
 
 
 int ajouter_ligne_fichier(const char *username, const char *filename, const char *line)
@@ -1232,6 +1197,44 @@ void traiter_msg(Utilisateur *utilisateur, char* target_username, char* msg)
     write_client(utilisateur->sock, "Message envoyé.\n");
 }
 
+void traiter_unwatch(Utilisateur *utilisateur)
+{
+    // Vérifiez si l'utilisateur est un spectateur
+    int found = 0;
+    for (int i = 0; i < nbSalons; i++)
+    {
+        Salon *salon = &salons[i];
+        for (int j = 0; j < salon->nbSpectateurs; j++)
+        {
+            if (salon->spectateurs[j] == utilisateur)
+            {
+                found = 1;
+
+                // Retirez le spectateur de la liste
+                for (int k = j; k < salon->nbSpectateurs - 1; k++)
+                {
+                    salon->spectateurs[k] = salon->spectateurs[k + 1];
+                }
+                salon->nbSpectateurs--;
+
+                // Notifiez l'utilisateur
+                write_client(utilisateur->sock, "Vous avez arrêté de regarder la partie.\n");
+
+                // Quittez les boucles après avoir supprimé l'utilisateur
+                break;
+            }
+        }
+        if (found)
+            break;
+    }
+
+    if (!found)
+    {
+        write_client(utilisateur->sock, "Erreur : Vous ne regardez aucune partie actuellement.\n");
+    }
+}
+
+
 void traiterMessage(Utilisateur *utilisateur, char *message)
 {
     if (utilisateur->isConnected)
@@ -1505,6 +1508,11 @@ void traiterMessage(Utilisateur *utilisateur, char *message)
                 write_client(utilisateur->sock, "Format incorrect. Utilisez : /msg [username] [message]\n");
             }
         }
+        else if (strcmp(message, "/unwatch") == 0)
+        {
+            traiter_unwatch(utilisateur);
+        }
+
         else
         {
             envoyerMessagePublic(utilisateur, message);
